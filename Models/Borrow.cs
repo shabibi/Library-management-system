883a06940cd419bc67050dc30dc64cacb2842e42
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 13:52:52 +0400

    Borrow model implemention

---
+++ Models/Borrow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Models
+{
+    public class Borrow
+    {
+        [Required] 
+        public DateTime BDate { get; set; }
+
+        public DateTime RDate { get; set; }
+        
+        public DateTime ActualDate { get; set; }
+
+        public bool IsReturned { get; set; }
+        [Range(0,5)]
+        public float Rating { get; set; }
+
+        [ForeignKey("Book")]
+        public int BID;
+        public Book Book { get; set; }
+
+        [ForeignKey ("User")]
+        public int UID { get; set; }
+
+        public User User { get; set; }
+        
+    }
+}
