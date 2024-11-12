432d16e601dc103153c8c01c420bff997dea2ebb
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 13:36:46 +0400

    User model implemention

---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int UID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string UName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Passcode { get; set; }
+
+        public enum Gender { male, female }
+        public Gender gender { get; set; }
+    }
+}
