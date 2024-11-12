f3985596482dc189729168863b6f2d06824170cf
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 12:25:36 +0400

    Admin model implemention

---
+++ Models/Admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Models
+{
+    public class Admin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string AName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+    }
+}
