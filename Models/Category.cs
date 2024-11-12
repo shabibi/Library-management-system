c17d88a6778df108ea6aafe7a9896565963b913f
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 13:23:06 +0400

    Category model implemention

---
+++ Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string CName { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int NumberOfBooks { get; set; }
+    }
+}
