b437798f014f1e53fd31dd30e61e586fecfb91a2
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 15:35:00 +0400

    AdminRepository

---
+++ Repositories/AdminRepository.cs
@@ -0,0 +1,58 @@
+﻿using LIBRARY.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Repositories
+{
+    public class AdminRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Admin> GetAllAdmin()
+        {
+            return _context.Admins.ToList();
+        }
+
+        public IEnumerable<Admin> GetAdminByName(string admin)
+        {
+            return _context.Admins
+                    .Where(e => e.AName == admin)
+                   .ToList();
+        }
+
+        public void InsertAdmin(Admin admin)
+        {
+            _context.Admins.Add(admin);
+            _context.SaveChanges();
+        }
+
+        public void UpdateAdminByName(string Name)
+        {
+            var admin = _context.Admins.FirstOrDefault(c => c.AName == Name);
+            if (admin != null)
+            {
+                _context.Admins.Update(admin);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteAdminById(int aid)
+        {
+            var admin = _context.Admins.Find(aid);
+            if (admin != null)
+            {
+                _context.Admins.Remove(admin);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
