6d42001dd9dd6b2248cf593b0bfe0685b1abc783
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 13:32:01 +0400

    Book model implemention

---
+++ Models/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string BTitle { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; }
+
+        [Range (1,int.MaxValue)]
+        public double Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int TotalCopies { get; set; }
+
+        [Range(0,int.MaxValue)]
+        public int BorrowedCopies { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int BorrowingPeriod { get; set; }
+
+        [ForeignKey ("Category")]
+        public int CID { get; set; }
+
+        public Category Category { get; set; }
+
+    
+    }
+}
