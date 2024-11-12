bfc13943d1d84974756fd3257eaeb1999d950e58
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-12 14:02:42 +0400

    Adding ApplicationDbContext

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using LIBRARY.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBRARY
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            options.UseSqlServer(" Data Source=(local); Initial Catalog= Library_System ; Integrated Security=true; TrustServerCertificate=True ");
+        }
+
+        public DbSet <Admin> Admins { get; set; }
+        public DbSet <User> Users { get; set; }
+        public DbSet <Book> Books { get; set; }
+        public DbSet <Category> Categories { get; set; }
+        public DbSet <Borrow> Borrows { get; set; }
+
+
+    }
+}
